da53fa5cbed55385783df3530fa37da7aa9703bd
Marco Upia <markremembers@gmail.com>
2024-04-05 12:09:48 -0400

    solved

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let reversed = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed.push(str[i])
+  }
+  return reversed.join('')
 }
 
 if (require.main === module) {
@@ -17,3 +22,8 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// function reverseString that takes a string
+      //loop over array while or for loop 
+      // then take the last index of the array and add it to a new array in that order
+      // return the new array as a string 
